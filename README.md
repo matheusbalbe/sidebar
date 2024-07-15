378f1332420b086b275b84348068bad239a55a8e
Matheus Balbe <matheusmarques1999@gmail.com>
2024-07-15 17:34:33 -0300

    descrição

---
+++ README.md
@@ -0,0 +1,29 @@
+<h1 align="center"> Sidebar </h1>
+
+<p align="center">O projeto consiste em uma "sidebar" como na imagem abaixo. Quando o usuário clica no ícone do menu, o estado da sidebar muda entre "aberto", mostrando ícones e texto e "fechado", mostrando apenas os ícones.
+
+<p align="center">
+Aberta
+  <img alt="projeto sidebar" src=".github/preview.png" width="100%">
+  </p>
+  <p align="center">
+Fechada <img alt="projeto sidebar" src=".github/preview2.png" width="100%">
+</p>
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- HTML
+- CSS
+- Javascript
+
+## 💻 Projeto
+
+- [Acesse o projeto finalizado, online](https://matheusbalbe.github.io/sidebar)
+
+---
+
+Feito por [Matheus Balbe](https://www.linkedin.com/in/matheusbalbe)
+<br>
+Projeto baseado no desafio da [RocketSeat]
--- script.js
+++ script.js
@@ -1,25 +1,24 @@
-let btn = document.querySelector("#btnMenu");
-let botoesDoMenu = document.querySelectorAll(".btn");
-let sidebar = document.querySelector(".sidebar");
-let searchBtn = document.querySelector(".icon_search");
+//Pegando os elementos
+const btn = document.querySelector("#btnMenu");
+const botoesDoMenu = document.querySelectorAll(".btn");
+const sidebar = document.querySelector(".sidebar");
+const searchBtn = document.querySelector(".icon_search");
 const logo = document.querySelector(".logo");
 const textoDosBotoes = document.querySelectorAll(".btnTexto");
 const perfil = document.querySelector(".profile_content");
 const fotoENome = document.querySelector(".profile");
-const inputDePesquisa = document.querySelector("#pesquisa");
+const inputDePesquisa = document.querySelector("#inputDePesquisa");
 const formDePesquisa = document.querySelector("#busca");
 
+// Função para retrair o menu
+// Quando o ícone para retrair for clicado, será adicionada a classe "reduzido" aos elementos e eles serão encurtados pelas configurações postas no css
 btn.addEventListener("click", function () {
-  sidebar.classList.toggle("ativo");
-  logo.classList.toggle("none");
-  textoDosBotoes.forEach((btna) => btna.classList.toggle("reduzido"));
+  sidebar.classList.toggle("reduzido");
+  logo.classList.toggle("reduzido");
+  textoDosBotoes.forEach((btn) => btn.classList.toggle("reduzido"));
   botoesDoMenu.forEach((btn) => btn.classList.toggle("reduzido"));
   inputDePesquisa.classList.toggle("reduzido");
   formDePesquisa.classList.toggle("reduzido");
   perfil.classList.toggle("reduzido");
   fotoENome.classList.toggle("reduzido");
 });
-
-// searchBtn.onclick = function () {
-//   sidebar.classList.toggle("active");
-// };
