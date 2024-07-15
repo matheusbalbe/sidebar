57290f0df9c3dd76cfec7d6e07a7f33f250d2a79
Matheus Balbe <matheusmarques1999@gmail.com>
2024-07-14 21:42:57 -0300

    correções e melhorias

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-let btn = document.querySelector("#btn");
+let btn = document.querySelector("#btnMenu");
+let botoesDoMenu = document.querySelectorAll(".btn");
 let sidebar = document.querySelector(".sidebar");
 let searchBtn = document.querySelector(".icon_search");
+const logo = document.querySelector(".logo");
+const textoDosBotoes = document.querySelectorAll(".btnTexto");
+const perfil = document.querySelector(".profile_content");
+const fotoENome = document.querySelector(".profile");
+const inputDePesquisa = document.querySelector("#pesquisa");
+const formDePesquisa = document.querySelector("#busca");
 
-btn.onclick = function() {
-    sidebar.classList.toggle("active");
-}
+btn.addEventListener("click", function () {
+  sidebar.classList.toggle("ativo");
+  logo.classList.toggle("none");
+  textoDosBotoes.forEach((btna) => btna.classList.toggle("reduzido"));
+  botoesDoMenu.forEach((btn) => btn.classList.toggle("reduzido"));
+  inputDePesquisa.classList.toggle("reduzido");
+  formDePesquisa.classList.toggle("reduzido");
+  perfil.classList.toggle("reduzido");
+  fotoENome.classList.toggle("reduzido");
+});
 
-searchBtn.onclick = function() {
-    sidebar.classList.toggle("active");
-}
+// searchBtn.onclick = function () {
+//   sidebar.classList.toggle("active");
+// };
